3d80e6a7578999624e43bd4fc9d39804249a44a9
orekish <orekish@163.com>
2021-03-22 16:53:47 +0800

    feat(elem-cascader): add sameLevelMerge prop

--- packages/elem-cascader/src/index.vue
+++ packages/elem-cascader/src/index.vue
@@ -15,9 +15,9 @@
     >
       <el-cascader
         ref="cascader"
-        v-model="inputVal"
+        :value="inputVal"
         v-bind="attrs"
-        v-on="$listeners"
+        v-on="listeners"
         style="width: 100%"
       >
         <cascader-slot
@@ -33,7 +33,7 @@ import pick from 'lodash/pick';
 import omit from 'lodash/omit';
 import camelCase from 'camelcase';
 import {
-  inputMixin, CustomRender,
+  inputMixin, CustomRender, TreeHelper,
 } from '@onemin-table/shared';
 import CascaderSlot from './components/cascader-slot';
 
@@ -89,6 +89,15 @@ export default {
 
     /**
      * @language=zh
+     * 级联选择器选项列表
+     */
+    options: {
+      type: Array,
+      default() { return []; },
+    },
+
+    /**
+     * @language=zh
      * 搜索无匹配项时列表渲染函数
      */
     emptySlotRender: {
@@ -131,11 +140,27 @@ export default {
       type: String,
       default: 'disabled',
     },
+
+    /**
+     * @language=zh
+     * 同一层级选中合并, 仅在multiple: true + emitPath: true + checkStrictly: false时有效
+     */
+    sameLevelMerge: {
+      type: Boolean,
+      default: false,
+    },
   },
 
   components: {
     CustomRender,
     CascaderSlot,
+  },
+
+  data() {
+    return {
+      treeHelper: null,
+      inputVal: null,
+    };
   },
 
   computed: {
@@ -158,6 +183,7 @@ export default {
       });
 
       const result = {
+        options: this.options,
         clearable: def('clearable'),
         filterable: def('filterable'),
         'collapse-tags': def('collapseTags'),
@@ -170,6 +196,11 @@ export default {
           label: this.labelKey,
           children: this.childrenKey,
           disabled: this.disabledKey,
+          // default value, 默认值
+          checkStrictly: false,
+          emitPath: true,
+          multiple: false,
+          expandTrigger: 'click',
 
           ...temp, // top level proxy attrs, 顶层代理属性
           ...this.$attrs?.props,
@@ -182,14 +213,73 @@ export default {
         ]),
       };
 
-      console.warn(result);
-
       return result;
     },
 
     innerPopoverAttrs() {
       return this.genDefaultPopoverAttrs('ot-cascader__popover--elem');
     },
+
+    listeners() {
+      return {
+        ...this.$listeners,
+        input: (val) => {
+          this.$emit('input', this.genOuterVal(val));
+        },
+        change: (val) => {
+          this.$emit('change', this.genOuterVal(val));
+        },
+      };
+    },
+
+    /**
+     * if can merge
+     * 是否可以合并
+     */
+    canMerge() {
+      const { multiple, emitPath, checkStrictly } = this.attrs?.props || {};
+      return this.sameLevelMerge && !!multiple && !!emitPath && !checkStrictly;
+    },
+
+    /**
+     * 叶子节点的路径值映射
+     * leaf node's path value map
+     */
+    leafPathMap() {
+      if (Array.isArray(this.options)) {
+        let result = {};
+        this.options.forEach((root) => {
+          const temp = this.treeHelper.genLeafPathMap(root);
+          result = {
+            ...result,
+            ...temp,
+          };
+        });
+
+        return result;
+      }
+
+      return {};
+    },
+  },
+
+  watch: {
+    options: {
+      handler() {
+        this.inputVal = this.genInnerVal();
+      },
+      deep: true,
+    },
+  },
+
+  created() {
+    // tree utils class, 多叉树工具类
+    this.treeHelper = new TreeHelper({
+      labelKey: this.labelKey,
+      valueKey: this.valueKey,
+      childrenKey: this.childrenKey,
+    });
+    this.inputVal = this.genInnerVal();
   },
 
   mounted() {
@@ -200,6 +290,38 @@ export default {
         this[key] = ref[key];
       });
     }
+  },
+
+  methods: {
+    /**
+     * 同一层级的合并分散到各个层级
+     * 例如[[1], [10, 12]]拆分为[[1, 10], [1, 12]]
+     */
+    genInnerVal() {
+      return this.canMerge
+        ? this.value[this.value.length - 1].map((val) => this.leafPathMap[val])
+        : this.value;
+    },
+
+    /**
+     * 分散在各个层级的数组合并到同一层级
+     * 例如[[1, 10], [1, 12]]合并为[[1], [10, 12]]
+     */
+    genOuterVal(val) {
+      if (this.canMerge && Array.isArray(val)) {
+        // longest path, 最长路径
+        const levelCount = Math.max(...val.map((e) => e.length));
+        const result = [];
+        for (let i = 0; i < levelCount; i += 1) {
+          const temp = val.map((e) => e[i]).filter(Boolean);
+          result.push([...new Set(temp)]);
+        }
+
+        return result;
+      }
+
+      return val;
+    },
   },
 };
 </script>
